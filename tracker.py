104f65435a3ab29c845748ddee70cfa8fa6ebfe8
aho <matsue4692@gmail.com>
2024-03-18 16:53:54 +0900

    Improve: tracker.py

--- tracker.py
+++ tracker.py
@@ -3,23 +3,23 @@ import math
 
 class Tracker:
     def __init__(self):
-        # Store the center positions of the objects
+        # オブジェクトの中心座標を格納する
         self.center_points = {}
-        # Keep the count of the IDs
-        # each time a new object id detected, the count will increase by one
+        # IDのカウントを保持する
+        # 新しいオブジェクトのIDが検出されるたびに、カウントが1ずつ増える
         self.id_count = 0
 
     def update(self, objects_rect):
         # Objects boxes and ids
         objects_bbs_ids = []
 
-        # Get center point of new object
+        #  新しいオブジェクトの中心点を取得
         for rect in objects_rect:
             x, y, w, h = rect
             cx = (x + x + w) // 2
             cy = (y + y + h) // 2
 
-            # Find out if that object was detected already
+            # そのオブジェクトがすでに検出されているかどうかを調べる
             same_object_detected = False
             for id, pt in self.center_points.items():
                 dist = math.hypot(cx - pt[0], cy - pt[1])
@@ -31,19 +31,19 @@ class Tracker:
                     same_object_detected = True
                     break
 
-            # New object is detected we assign the ID to that object
+            # 新しいオブジェクトが検出されたら、そのオブジェクトにIDを割り当てる
             if same_object_detected is False:
                 self.center_points[self.id_count] = (cx, cy)
                 objects_bbs_ids.append([x, y, w, h, self.id_count])
                 self.id_count += 1
 
-        # Clean the dictionary by center points to remove IDS not used anymore
+        # もう使われていないIDSを削除するために、中心点によって辞書をきれいにする
         new_center_points = {}
         for obj_bb_id in objects_bbs_ids:
             _, _, _, _, object_id = obj_bb_id
             center = self.center_points[object_id]
             new_center_points[object_id] = center
 
-        # Update dictionary with IDs not used removed
+        # 使用されていないIDを削除して辞書を更新
         self.center_points = new_center_points.copy()
         return objects_bbs_ids
